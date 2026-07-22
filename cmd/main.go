70f472d177fc6194d8869ab35fa0664208ba39d4
Edison Agurto <edisonagurto@yahoo.com>
2026-07-21 20:58:59 -0500

    Se carga modelo

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/efad/prueba2-ordenes/internal/delivery/graphql"
+	"github.com/vektah/gqlparser/v2/ast"
 )
 
 type healthResponse struct {
@@ -19,9 +27,13 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", healthHandler)
+	mux.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	mux.Handle("/query", newGraphQLHandler())
 
 	addr := ":" + port
 	log.Printf("servidor escuchando en http://localhost%s", addr)
+	log.Printf("health check: http://localhost%s/health", addr)
+	log.Printf("graphql playground: http://localhost%s/", addr)
 	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatalf("error al iniciar servidor: %v", err)
 	}
@@ -37,4 +49,21 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(healthResponse{Status: "ok"})
+}
+
+func newGraphQLHandler() http.Handler {
+	srv := handler.New(graphql.NewExecutableSchema(graphql.Config{
+		Resolvers: &graphql.Resolver{},
+	}))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	return srv
 }
---
+++ internal/delivery/graphql/resolver.go
@@ -0,0 +1,5 @@
+package graphql
+
+// Resolver es la raiz de inyeccion de dependencias para los resolvers GraphQL.
+// Los use cases se agregaran en commits posteriores.
+type Resolver struct{}
